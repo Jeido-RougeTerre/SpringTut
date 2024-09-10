a05036942823bafa3f3fcebbb082a2d72ae888b4
Teo Gontard <teo.gontard@gmail.com>
2024-09-10 15:31:06 +0200

    Ex2 finished

--- src/main/java/com/jeido/exercisespring/controllers/StudentController.java
+++ src/main/java/com/jeido/exercisespring/controllers/StudentController.java
@@ -5,8 +5,7 @@ import com.jeido.exercisespring.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,16 +18,20 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+        studentService.createStudent("Doe", "John", 33, "john.doe@lemail.com");
     }
 
-    @RequestMapping("/student")
-    public String index(Model model) {
-        return search(null, model);
+    @RequestMapping("/student/register")
+    public String register(Model model) {
+        Student student = Student.builder().name("").surname("").age(0).email("").build();
+        model.addAttribute("student", student);
+        return "student/register";
     }
 
-    @RequestMapping("/student/inscription")
-    public String inscription() {
-        return "student/inscription";
+    @PostMapping("/student/register")
+    public String addStudent(@ModelAttribute("student") Student student) {
+        studentService.createStudent(student.getSurname(), student.getName(), student.getAge(), student.getEmail());
+        return "redirect:/student";
     }
 
     @RequestMapping("/student/{id}")
@@ -38,16 +41,26 @@ public class StudentController {
         return "student/details";
     }
 
-    @RequestMapping("/student/search")
-    public String search(@PathVariable(value = "name", required = false)String name ,Model model) {
-        if (name != null) {
-            List<Student> students = studentService.getStudentsByName(name);
+    @RequestMapping("/student")
+    public String search(@RequestParam(name = "search", required = false)String search , Model model) {
+        List<Student> students;
+        if (search != null) {
+            students = studentService.getStudentsByName(search);
             model.addAttribute("students", students);
-            model.addAttribute("searchedName", name);
+            model.addAttribute("searchedName", search);
             model.addAttribute("mode", "search");
         } else {
+            students = studentService.getAllStudents();
+            model.addAttribute("students", students);
             model.addAttribute("mode", "list");
         }
         return "student/index";
     }
+
+    @PostMapping("/student/search")
+    public String searchPost(@ModelAttribute("search")String name) {
+        return "redirect:/student?search=" + name;
+    }
+
+
 }
